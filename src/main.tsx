0163c151b73b670e0da0636a5a502888e43aeeb9
Ivan <imkolganov@gmail.com>
2026-08-08 20:13:19 +0300

    Bake brand name into the HTML title at build time.
    Stops the tab flashing the Vite scaffold title before JS sets VITE_BRAND_NAME.

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { applyTheme, resolveInitialTheme } from './brand/themeUtils'
-import { brandConfig } from './brand/config'
 import './index.css'
 import App from './App.tsx'
 
 applyTheme(resolveInitialTheme())
-document.title = brandConfig.name
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
